a983ce057bbdcd468fdbd081f3dab35052710bff
Marten Veldthuis <marten@veldthuis.com>
2014-07-12 14:46:39 +0200

    Remove example app

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,36 +4,11 @@ var React = window.React = require('react'),
     Timer = require("./ui/Timer");
     mountNode = document.getElementById("quby-app");
 
-var TodoList = React.createClass({
-  render: function() {
-    var createItem = function(itemText) {
-      return <li>{itemText}</li>;
-    };
-    return <ul>{this.props.items.map(createItem)}</ul>;
-  }
-});
-var TodoApp = React.createClass({
-  getInitialState: function() {
-    return {items: [], text: ''};
-  },
-  onChange: function(e) {
-    this.setState({text: e.target.value});
-  },
-  handleSubmit: function(e) {
-    e.preventDefault();
-    var nextItems = this.state.items.concat([this.state.text]);
-    var nextText = '';
-    this.setState({items: nextItems, text: nextText});
-  },
+var QubyApp = React.createClass({
   render: function() {
     return (
       <div>
         <h3>TODO</h3>
-        <TodoList items={this.state.items} />
-        <form onSubmit={this.handleSubmit}>
-          <input onChange={this.onChange} value={this.state.text} />
-          <button>{'Add #' + (this.state.items.length + 1)}</button>
-        </form>
         <Timer />
       </div>
     );
@@ -41,5 +16,5 @@ var TodoApp = React.createClass({
 });
 
 
-React.renderComponent(<TodoApp />, mountNode);
+React.renderComponent(<QubyApp />, mountNode);
 
