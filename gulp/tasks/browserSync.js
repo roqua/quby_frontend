74e4355dccac1eb89a7769c07cec3f4ba73a5777
Marten Veldthuis <marten@veldthuis.com>
2014-07-24 12:47:56 +0200

    Use bower version of vendor libs

--- gulp/tasks/browserSync.js
+++ gulp/tasks/browserSync.js
@@ -3,6 +3,8 @@ var gulp        = require('gulp');
 
 gulp.task('browserSync', ['build'], function() {
 	browserSync.init(['build/**'], {
+    open: false,      // dont open browser always, since it might still be open
+    ghostMode: false, // syncing clicks and form changes doesnt work well with react.
 		server: {
 			baseDir: 'build'
 		}
